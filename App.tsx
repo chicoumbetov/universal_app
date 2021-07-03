cd4d2336b9ff40ba9201b24b779a2e8b470bd686
Shynggys Umbetov <shynggys@web-premiere.fr>
2021-07-03 14:40:06 +0200

    Base code with assets and fonts

--- App.tsx
+++ App.tsx
@@ -1,24 +1,56 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createStackNavigator } from "@react-navigation/stack";
+// import { LogBox } from "react-native";
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+// import useColorScheme from './hooks/useColorScheme';
+// import Navigation from './navigation';
+import { Routes } from './src/Routes';
+
+// import { assets as globalAssets } from "./src/components/assets";
+// import { assets as pizzaAsset } from "./src/PizzaApp/assets";
+
+import Pizza from './src/PizzaApp/PizzaApp'
+import LoadAssets from './src/components/LoadAssets';
+
+/**
+const fonts = {
+  "SFProDisplay-Bold": require("./assets/fonts/SFPro/SF-Pro-3Display-Bold.otf"),
+  "SFProDisplay-Semibold": require("./assets/fonts/SFPro/SF-Pro-Display-Semibold.otf"),
+  "SFProDisplay-Regular": require("./assets/fonts/SFPro/SF-Pro-Display-Regular.otf"),
+  "SFProDisplay-Medium": require("./assets/fonts/SFPro/SF-Pro-Display-Medium.otf"),
+  Antpolt: require("./assets/fonts/antpoltsemicond-bolditalic.ttf"),
+};
+*/
+
+// const assets = [...globalAssets, ...pizzaAsset];
+const Stack = createStackNavigator<Routes>();
+
+const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="PizzaChallenge"
+      component={Pizza}
+      options={{
+        title: "🍕 Pizza Challenge",
+      }}
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <LoadAssets>
+        <AppNavigator />
+      </LoadAssets>
     );
   }
 }
